4c52fa56828a087a09e8eae89bab4e15bea665a3
crippa1337 <cristopher.torgrip@elev.ga.ntig.se>
2022-10-30 00:39:50 +0200

    commented dvd bounce and removed dvdcounter

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,13 +5,14 @@ let elemOldText = document.getElementById("old-screen");
 let displayText = "";
 let oldText = "";
 
-// for the dvd bounce
+// for dvd bounce
 let main = document.getElementById("main");
-let x_incr = 2;
-let y_incr = 2;
+let x_incr = 1;
+let y_incr = 1;
 let isBouncing = false;
 let dvdCounterText = document.getElementById("dvd-counter")
 let dvdStopCount = 0;
+let bouncer;
 
 const titles = ["CALCULATOR", "KALKULATOR", "KALCULATOR", "CALKULATOR"]
 
@@ -99,9 +100,7 @@ function functionClick(operator) {
     else if (operator == "DVD") {
         if (isBouncing == true) {
             isBouncing = false;
-            dvdStopCount += 1;
-            dvdCounterText.innerText = "DVD Stop Count: " + dvdStopCount;
-            dvdCounterText.style.display = "block";
+            clearInterval(bouncer);
         }
         else {
             dvdInit();
@@ -123,7 +122,6 @@ function removeUnderScore() {
 
 //Make the DIV element draggagle:
 dragElement(document.getElementById("main"));
-
 function dragElement(element) {
     let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     if (document.getElementById(element.id + "header")) {
@@ -165,11 +163,15 @@ function dragElement(element) {
     }
 }
 
+// dvdInit() is called when the DVD button is pressed
 function dvdInit() {
-    setInterval(frame, 5);
+    // calls frame() every 5ms
+    bouncer = setInterval(frame, 5);
+    // sets isBouncing to true so that the dvd button can be used to stop the bouncing
     isBouncing = true;
 }
 
+// handle_collision() is called everytime frame() is called to check if the dvd is colliding with the edges of the screen
 function handle_collision() {
     let main_height = main.offsetHeight;
     let main_width = main.offsetWidth;
@@ -178,17 +180,23 @@ function handle_collision() {
     let win_height = window.innerHeight;
     let win_width = window.innerWidth;
 
+    // if the dvd is colliding with the top or bottom edge of the screen, invert x_incr
     if (left <= 0 || left + main_width >= win_width) {
         x_incr = ~x_incr + 1;
     }
+
+    // if the dvd is colliding with the top or bottom edge of the screen, invert y_incr
     if (top <= 0 || top + main_height >= win_height) {
         y_incr = ~y_incr + 1;
     }
 }
 
 function frame() {
+    // if isBouncing is false, frame() will not be called, making the calculator stop bouncing
     if (isBouncing == true) {
+        // calls handle_collision() to check if the dvd is colliding with the edges of the screen
         handle_collision();
+        // moves the dvd by x_incr and y_incr pixels
         main.style.top = `${main.offsetTop + y_incr}px`;
         main.style.left = `${main.offsetLeft + x_incr}px`;
     }
